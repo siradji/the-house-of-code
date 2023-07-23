88cdf39ea873aad1b63272e5a16942554ce3df59
Suraj Auwal <siradjiawoual@gmail.com>
2023-07-23 16:17:04 +0100

    chore: install google tags
    Signed-off-by: siradji <siradjiawoual@gmail.com>

---
+++ src/app/inquire/page.tsx
@@ -0,0 +1,64 @@
+import {IoPeople} from "react-icons/io5";
+import {MdOutlineHandshake, MdOutlineSecurity} from "react-icons/md";
+import {HiLightBulb} from "react-icons/hi";
+import {ContactForm} from "@/components/ContactForm";
+
+export default function InquirePage () {
+    return (
+        <div className="px-12 lg:px-20 xl:px-32 py-10">
+            <div className="flex flex-col w-full items-center justify-center">
+                <h1 className="text-5xl text-branding-blue-500 font-bold">Let's Collaborate!</h1>
+                <p className="text-sm text-black font-bold w-full md:w-1/2 text-center leading-[25px] mt-4">At The House Of Code, collaboration is at the heart of everything we do. By working together, we can unlock the full potential of your organization and craft exceptional digital experiences that leave a lasting impact</p>
+            </div>
+            <div className="bg-branding-blue-500 p-6 rounded-[20px] my-10">
+                <div className="flex flex-row w-full justify-center">
+                    <h1 className="text-3xl text-branding-orange-500">Why Work With Us?</h1>
+                </div>
+                <WhyWorkWithUs />
+            </div>
+
+            <div className="flex flex-col w-full items-center justify-center my-4">
+                <h1 className="text-2xl text-branding-blue-500 font-bold">Let's Get Started!</h1>
+                <p className="text-sm md:text-lg text-black font-bold w-full md:w-1/2 text-center leading-[25px] mt-4">
+                    Fill out the form and tell us a bit about your project or requirements. We'll get back to you in a few hours to schedule a discussion and explore how The House Of Code can bring your digital dreams to life. Together, let's embark on a transformative journey towards digital success!
+                </p>
+            </div>
+            <div className="flex rounded-[20px] justify-center ">
+                <ContactForm />
+            </div>
+        </div>
+    )
+}
+
+
+function WhyWorkWithUs () {
+    return (
+        <div className="flex items-center flex-wrap">
+            <div className="flex flex-col w-full md:w-1/3 my-3">
+                <div className="flex flex-row items-center mb-2">
+                    <IoPeople size={40} className="text-white" />
+                    <h1 className="text-xl font-bold text-white">Expertise</h1>
+                </div>
+                <p className="text-sm text-white  w-[80%]">Our team of skilled professionals brings a wealth of expertise and creativity to every project.</p>
+            </div>
+            <div className="flex flex-col w-full md:w-1/3 my-3">
+                <div className="flex flex-row items-center mb-2">
+                    <HiLightBulb size={40} className="text-white" />
+                    <h1 className="text-xl font-bold text-white">Innovation</h1>
+                </div>
+                <p className="text-sm text-white w-[80%]">
+                    Stay ahead of the curve with our cutting-edge technology solutions.
+                </p>
+            </div>
+            <div className="flex flex-col w-full md:w-1/3 my-3">
+                <div className="flex flex-row items-center mb-2">
+                    <MdOutlineHandshake size={40} className="text-white" />
+                    <h1 className="text-xl font-bold text-white">Customer-Centric</h1>
+                </div>
+                <p className="text-sm text-white  w-[80%]">
+                    Your vision is our focus. We tailor our services to meet your unique needs and goals.
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,135 @@
+"use client"
+import { FormEventHandler, HTMLInputTypeAttribute, useState} from 'react'
+
+interface ContactFormI  {
+    fullName: string
+    phoneNumber: string
+
+    email: string
+
+    budget: string,
+
+    idea: string
+
+    companyName: string
+}
+
+export function ContactForm () {
+    const [formState, setFormState] = useState({
+        fullName: '',
+        phoneNumber: '',
+        email: '',
+        budget: '',
+        idea: '',
+        companyName: '',
+    })
+
+
+    const handleFormChange = (name:  keyof ContactFormI, value: string ): void => {
+            setFormState((prev) => ({...prev, [name]: value}))
+    }
+
+
+    const handleFormSubmit = (e:FormEventHandler<HTMLFormElement> | any): void => {
+        e.preventDefault()
+
+        console.log(e)
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit} className="w-full md:w-1/2">
+            <FormControl
+                value={formState.fullName}
+                name="fullName"
+                label="Full name"
+                onChange={handleFormChange}
+                placeholder="Musa Sani"
+            />
+            <FormControl
+                value={formState.phoneNumber}
+                name="phoneNumber"
+                label="Phone Number"
+                onChange={handleFormChange}
+                placeholder="080 200 400000"
+            />
+            <FormControl
+                value={formState.companyName}
+                name="companyName"
+                label="Company Name"
+                onChange={handleFormChange}
+                placeholder="HSCB Bank"
+            />
+            <FormControl
+                value={formState.companyName}
+                name="email"
+                label="Email"
+                onChange={handleFormChange}
+                placeholder="mycompany@gmail.com"
+            />
+            <FormControl
+                value={formState.companyName}
+                name="budget"
+                label="What is your budget?"
+                onChange={handleFormChange}
+                type="number"
+            />
+            <FormControlTextArea
+                value={formState.idea}
+                name="idea"
+                label="Tell us about your project/idea"
+                onChange={handleFormChange}
+            />
+            <button className="bg-branding-orange-500 rounded-[20px] text-white shadow-2xl py-3 px-4" type="submit">
+                Submit form
+            </button>
+        </form>
+    )
+
+}
+
+
+interface FormControlI {
+   onChange: (name: keyof ContactFormI, value: string) => void,
+    name: keyof ContactFormI
+    value: string
+    label: string
+    placeholder?: string
+
+    type?: HTMLInputTypeAttribute
+}
+
+function FormControl (props: FormControlI) {
+      return (
+          <div className="flex w-full my-5">
+              <label className="text-black text-sm font-bold flex flex-col w-full">
+                  {props.label}
+                  <input
+                      type={props.type}
+                      placeholder={props.placeholder}
+                      name={props.name}
+                      value={props.value}
+                      onChange={({target: {value, name}}) => props.onChange(name as any, value) }
+                      className="active:border-[1px] active:border-branding-blue-500 border-gray-300 mt-2 border-[1px] rounded-[5px] py-3 px-2 text-black text-base font-bold"
+                  />
+              </label>
+          </div>
+      )
+}
+
+
+function FormControlTextArea (props: FormControlI) {
+    return (
+        <div className="flex w-full my-5">
+            <label className="text-black text-sm font-bold flex flex-col w-full">
+                {props.label}
+                <textarea
+                    placeholder={props.placeholder}
+                    name={props.name}
+                    value={props.value}
+                    onChange={({target: {value, name}}) => props.onChange(name as any, value) }
+                    className="active:border-[1px] active:border-branding-blue-500 border-gray-300 mt-2 border-[1px] rounded-[5px] py-3 px-2 text-black text-base font-bold"
+                />
+            </label>
+        </div>
+    )
+}
